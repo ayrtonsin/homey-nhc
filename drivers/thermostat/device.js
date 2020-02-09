8a38a6f79dee5105b04a6f44d3279eb6fd5881b1
Ayrton Sinnaeve <ayrton@d-n.be>
2020-02-09 15:52:21 +0100

    implement thermostat mode

--- drivers/thermostat/device.js
+++ drivers/thermostat/device.js
@@ -2,38 +2,110 @@
 
 const Homey = require('homey');
 const niko = require('niko-home-control');
+const listThermostatCmd = '{"cmd": "listthermostat"}';
+const execCmd = (id, mode) => {
+    return '{"cmd": "executethermostat", "id":' + id + ', "mode": ' + mode + '}';
+  };
 
 class MyDevice extends Homey.Device {
 	
 	onInit() {
-		this.log('Device init');
+		this.log('Thermostat Device init');
 		this.log('Name:', this.getName());
 		this.log('Class:', this.getClass());
         this.log('id:', this.getData().id);
 
         
-		//receive updates from niko: when thermostat is changed.
-		this.trackNhcEvent(this);          
+		//check for thermostat state in the NHC controller in an interval
+        this.intervalObj = setInterval(this.trackNhcThermostats, 30000,this);
+        this.registerCapabilityListener('thermostat_mode', this.onModeChange.bind(this));
+        this.log('Done init thermostat device');
 	}
+	
+	nhcProgramToMode(nhcVal){
+	    var mode = null;
+	    switch(nhcVal) {
+	        case 0:
+	        case 1:
+	        case 2:
+	            mode = "heat";
+	          break;
+	        case 3:
+                mode = "off";
+              break;
+	        case 4:
+                mode = "cool";
+              break;
+	        case 5:
+                mode = "auto";
+              break;
+	        default:
+	            mode = "auto";
+	      }
+	    return mode;
+	}
+	
+	homeyModeToNhc(val){
+        var mode = null;
+        switch(val) {
+            case "heat":
+                mode = 0;
+            case "auto":
+                mode = 5;
+            case "cool":
+                mode = 4;
+              break;
+            case "off":
+                mode = 3;
+              break;            
+            default:
+                mode = 5;
+          }
+        return mode;
+    }
     
 
-    trackNhcEvent(t){
-        this.log('tracking nhc event...');
-        var that = this;
-        this._connectNiko(this.getSetting("ip"));
-        niko.events.on('listthermostat', (event) => {
-            that.log('nhc event received...');
-            event.data.forEach(function (d) {
-              that.log("same id??",d);
-              var data = that.getData();
-              if(d.id == data.id){
-                that.log('updating device value', d.id);
-                
-              }
-
+	trackNhcThermostats(that){
+        that.log('tracking nhc thermostats...');
+        var ip = that.getSetting("ip");
+        if (ip != null){
+            that._connectNiko(ip);
+            niko.command(listThermostatCmd)
+                .then(function(response){
+                    that.log('nhc thermostat response...');
+                    response.data.forEach(function (d) {
+                        that.log("same id??",d);
+                        var data = that.getData();
+                        if(d.id == data.id){
+                            var measured = d.measured / 10;
+                            var setpoint = d.setpoint / 10;
+                            var mode = that.nhcProgramToMode(d.mode);
+                            that.log('updating device value', d.id);
+                            that.setCapabilityValue('measure_temperature', measured);
+                            that.setCapabilityValue('target_temperature', setpoint);
+                            that.setCapabilityValue('thermostat_mode', mode);
+                    }
+                });
             });
-          });          
+        }
     }
+	
+	
+	//this method is called when Homey requests a new temperature mode
+	async onModeChange(value, opts) {
+	    var that = this;
+	    var data = this.getData();
+	    var nhcMode = this.homeyModeToNhc(value);
+	    this.log('switching thermostat mode value::' +  nhcMode);
+        var data = this.getData();
+        niko.command(execCmd(data.id, nhcMode))
+            .then(function(response){
+                that.log('execute thermostat response...');
+                that.log(response);
+                that.setCapabilityValue('thermostat_mode', value);
+                
+            });
+	}
 
 	_connectNiko (ip){
 	  console.log("connecting niko");
@@ -41,7 +113,7 @@ class MyDevice extends Homey.Device {
 		ip: ip, // "192.168.1.2",//this.settings.ip,
 		port: 8000,
 		timeout: 20000,
-		events: true
+		events: false
 	  });
 	}	
 	
